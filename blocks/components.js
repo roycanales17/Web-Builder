08e91ce6610d25253b936a7d30fa769cf16aeec1
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-07-15 20:03:03 +0800

    added new banner 5

--- blocks/components.js
+++ blocks/components.js
@@ -4,6 +4,7 @@ import banner1 from '../blocks/banners/banner-1.js';
 import banner2 from '../blocks/banners/banner-2.js';
 import banner3 from '../blocks/banners/banner-3.js';
 import banner4 from '../blocks/banners/banner-4.js';
+import banner5 from '../blocks/banners/banner-5.js';
 import navbar from '../blocks/navbars/navbar-1.js';
 
 export const templateComponents = {
@@ -15,7 +16,8 @@ export const templateComponents = {
 		banner1,
 		banner2,
 		banner3,
-		banner4
+		banner4,
+		banner5
 	],
 	'template-navbar': [
 		navbar,
